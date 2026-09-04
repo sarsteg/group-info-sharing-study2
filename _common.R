b81f1cd0e1a08b502c4a478e39d7bb58a59cb092
sarsteg <85451611+sarsteg@users.noreply.github.com>
2026-09-04 10:16:26 -0500

    template for common functions and libraries

---
+++ _common.R
@@ -0,0 +1,12 @@
+# Libraries ----------------------------------------------
+
+library(psych)
+library(dplyr)
+library(tidyr)
+library(haven)
+library(descr)
+library(ggplot2)
+library(readr)
+
+# Functions ----------------------------------------------
+
